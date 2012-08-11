a63d14e472fe874a170c27247b715c26903b52c1
Oz Elentok <oz.elen@gmail.com>
2012-08-11 21:34:22 +0300

    added optional packages - will be installed with confirmation from user

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-#
+
+# =======================================
+# Installation Flags for extra packages
+gimpFlag="y"
+audacityFlag="y"
+virtBoxFlag="y"
+lampFlag="y"
+
+# =======================================
+# Extra packages prompt
+read -p "Do you want to install GIMP - Image Editor [Y/n]?" gimpFlag
+read -p "Do you want to install Audacity - Audio Editor [Y/n]?" audacityFlag
+read -p "Do you want to install VirtualBox - Virtualizion software [Y/n]?" virtBoxFlag
+read -p "Do you want to Install LAMP - Web Server software [Y/n]?" lampFlag
+
 # ========================================
 # Base packages
 sudo apt-get update
@@ -7,9 +21,36 @@ sudo apt-get install -y vim-gnome \
   ctags keepass2 htop samba \
   libpam-smbpass pysdm unrar \
   network-manager-gnome \
-  krusader vlc smplayr \
-	deluge gimp qt4-config
+  krusader vlc smplayer
 
+# Extra packaes(optional)
+
+if [[ $gimpFlag == [Yy]* ]]
+	echo ""
+	echo "====================="
+	echo "Installing Gimp"
+	sudo apt-get install -y gimp
+fi
+if [[ $audacityFlag == [Yy]* ]]
+	echo ""
+	echo "====================="
+	echo "Installing Audacity"
+	sudo apt-get install -y audacity
+
+fi
+if [[ $virtBoxFlag == [Yy]* ]]
+	echo ""
+	echo "====================="
+	echo "Installing VirtualBox"
+	sudo apt-get install -y virtualbox
+fi
+if [[ $lampFlag == [Yy]* ]]
+	echo ""
+	echo "====================="
+	echo "Installing LAMP"
+	sudo apt-get install -y taskel
+	sudo taskel install lamp-server
+fi
 # ========================================
 # Modules
 sudo ./modules/google.sh
@@ -17,4 +58,7 @@ sudo ./modules/google.sh
 
 #=========================================
 # Configure bash tab-completion case insensitive
+echo ""
+echo "=============================="
+echo "Enabling case-insensitve auto-completion
 echo "set completion-ignore-case on" | sudo tee -a /etc/inputrc
