3dfad5a84df3a33d582048f6db01bf50f8ec6da1
Oz Elentok <oz.elen@gmail.com>
2013-02-20 19:27:17 +0200

    fixed vim and bash files copying

--- modules/bash/config.sh
+++ modules/bash/config.sh
@@ -12,8 +12,11 @@ echo "set completion-ignore-case on" | sudo tee -a /etc/inputrc
 # set terminal colors to 256
 echo "export TERM=xterm-256color" | sudo tee -a ~/.bashrc
 
+# script dir
+DIR=$(dirname "${BASH_SOURCE[0]}")
+DIR=$(cd -P $DIR && pwd)
 # add custom functions
-cat ./functions.sh >> ~/.bashrc
+cat $DIR/functions.sh >> ~/.bashrc
 
 # add custom aliases
-cat ./aliases.sh >> ~/.bash_aliases
+cat $DIR/aliases.sh >> ~/.bash_aliases
--- modules/vim/vim.sh
+++ modules/vim/vim.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-cp ./.vimrc ~/.vimrc
+DIR=$(dirname "${BASH_SOURCE[0]}")
+DIR=$(cd -P $DIR && pwd)
+cp $DIR/.vimrc ~/.vimrc
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
