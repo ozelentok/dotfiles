57286f05513b52248e62ab6fd8f73712f9a168a4
Oz Elentok <oz.elen@gmail.com>
2013-03-14 00:02:42 +0200

    added lx alias - print folders first

--- modules/bash/aliases.sh
+++ modules/bash/aliases.sh
@@ -1,4 +1,6 @@
-alias pss='csvtool readable ~/Documents/schedule.csv'
 alias pwf='sudo poweroff'
 alias rbt='sudo reboot'
+alias pss='csvtool readable ~/Documents/schedule.csv'
+# print folders first and then files
+alias lx='ls -X'
 alias todo='~/.todotxt/todo.sh'
