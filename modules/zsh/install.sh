eaa1beeb1e6597f9763ebbfa1626239ccbe3c3a6
Oz Elentok <oz.elen@gmail.com>
2013-05-19 23:46:39 +0300

    added patched fonts and vim 'fancy' powerline symbols

--- modules/zsh/install.sh
+++ modules/zsh/install.sh
@@ -17,4 +17,10 @@ git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.zsh/synt
 ln -s $DIR/zshrc $HOME/.zshrc
 ln -s $DIR/aliases.zsh $HOME/.zsh/aliases.zsh
 ln -s $DIR/zspower.py $HOME/.zsh/zspower.py
-sudo chsh -s $(which zsh)
+echo "Zsh Settings Configuration"
+sudo usermod -s $(which zsh) $USER
+echo ""
+echo "=============================="
+echo "Installing patched ubuntu mono fonts"
+git clone https://github.com/pdf/ubuntu-mono-powerline-ttf.git ~/.fonts/ubuntu-mono-powerline-ttf
+fc-cache -vf
