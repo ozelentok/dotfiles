ebf2b4b2a446d43d6e112e2d7355c5690ae17999
Oz Elentok <oz.elen@gmail.com>
2013-05-19 18:02:11 +0300

    fixed vim color

--- modules/vim/install.sh
+++ modules/vim/install.sh
@@ -3,6 +3,7 @@
 DIR=$(dirname "${BASH_SOURCE[0]}")
 DIR=$(cd -P $DIR && pwd)
 ln -sf $DIR/vimrc $HOME/.vimrc
+mkdir $HOME/.vim/colors
 cp $DIR/obsidian.vim $HOME/.vim/colors/obsidian.vim
 git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
 vim +BundleInstall +qall
