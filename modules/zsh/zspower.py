92ac10ba4df430f6adc594b7282e4dfd152586e4
Oz Elentok <oz.elen@gmail.com>
2013-09-08 15:20:27 +0300

    faster shell prompt

--- modules/zsh/zspower.py
+++ modules/zsh/zspower.py
@@ -15,17 +15,17 @@ symbols = {
 
 class Painter:
 	def __init__(self):
-		self.sep = ' ' + symbols['thinSep'] + ' '
+		self.sep = u' ' + symbols['thinSep'] + u' '
 		#self.colorTemplate = unicode('\\[\\e[{0};5;{1}m\\]{2}')
 		self.colorTemplate = unicode('%{{[{0};5;{1}m%}}{2}')
 		#self.endColorTemplate = unicode('\\[\\e[0m\\]')
 		self.endColorTemplate = unicode('%{[0m%}')
 	def resetColor(self):
 		return self.endColorTemplate;
-	def drawBG(self, text = '', color = colors['pathBG']):
+	def drawBG(self, text = u'', color = colors['pathBG']):
 		return self.colorTemplate.format(
 				48, color, text);
-	def drawFG(self, text = '', color = colors['pathFG']):
+	def drawFG(self, text = u'', color = colors['pathFG']):
 		return self.colorTemplate.format(
 				38, color, text);
 
@@ -33,19 +33,20 @@ def main():
 	painter = Painter()
 	home = os.getenv('HOME')
 	cwd = os.getcwd() + ' '
-
+	promptString = unicode('')
 	if cwd.find(home) == 0:
 		cwd = cwd.replace(home, ' ~', 1)
-	cwd = cwd.replace('/', painter.sep);
-	sys.stdout.write(painter.drawBG());
-	sys.stdout.write(painter.drawFG(cwd).encode('utf-8'));
-	sys.stdout.write(painter.resetColor());
-	sys.stdout.write(painter.drawFG(symbols['sep'], colors['pathBG']).encode('utf-8'));
-	sys.stdout.write(painter.drawBG(color=colors['cmdBG']));
-	sys.stdout.write(painter.drawFG('\n $ ').encode('utf-8'));
-	sys.stdout.write(painter.resetColor());
-	sys.stdout.write(painter.drawFG(symbols['sep'], colors['cmdBG']).encode('utf-8'));
-	sys.stdout.write(painter.resetColor());
+	cwd = cwd.replace('/', painter.sep)
+	promptString += painter.drawBG()
+	promptString += painter.drawFG(cwd)
+	promptString += painter.resetColor()
+	promptString += painter.drawFG(symbols['sep'], colors['pathBG'])
+	promptString += painter.drawBG(color=colors['cmdBG'])
+	promptString += painter.drawFG(u'\n $ ')
+	promptString += painter.resetColor()
+	promptString += painter.drawFG(symbols['sep'], colors['cmdBG'])
+	promptString += painter.resetColor()
+	sys.stdout.write(promptString.encode('utf-8'))
 
 if __name__ == '__main__':
 	main()
