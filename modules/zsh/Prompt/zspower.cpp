2f883f2399f4f0b0910ad248a66b445f7956a859
Oz Elentok <oz.elen@gmail.com>
2014-03-21 15:49:04 +0200

    Converted zspower(shell prompt) implementation from Python to C++

---
+++ modules/zsh/Prompt/zspower.cpp
@@ -0,0 +1,102 @@
+// Filename: zspower.cpp
+// Author: Oz Elentok
+// Description: Color prompt for zsh
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
+#include <unistd.h>
+#include <algorithm>
+
+using std::string;
+using std::stringstream;
+
+static const string orgSeperator("/");
+static const string seperator("⮀");
+static const string thinSep(" ⮁ ");
+static const string homeStart(" ~");
+static const string promptStart("\n $ ");
+static const string colorTemplateBG("%{[48;5;m%}");
+static const string colorTemplateFG("%{[38;5;m%}");
+static const string resetColors("%{[0m%}");
+static const char * const homeEnvironment = "HOME";
+static const size_t colorInsertionIndex = 9;
+static const size_t averagePromptSize = 150;
+
+namespace colors {
+	static const string pathFG("255");
+	static const string pathBG("25");
+	static const string promptBG("22");
+}
+
+string startColorFG(const string & fgColor) {
+	string fgText(colorTemplateFG);
+	fgText.insert(colorInsertionIndex,fgColor);
+	return fgText;
+}
+
+string startColorBG(const string & bgColor) {
+	string bgText(colorTemplateBG);
+	bgText.insert(colorInsertionIndex, bgColor);
+	return bgText;
+}
+
+void replaceSeperators(string & cwd) {
+	size_t index = 0;
+	bool is_root = false;
+	is_root = (orgSeperator.length() == cwd.length());
+	while (true) {
+		 index = cwd.find(orgSeperator, index);
+		 if (index == std::string::npos) {
+			 break;
+		 }
+		 cwd.replace(index, orgSeperator.length(), thinSep);
+		 index += thinSep.length();
+	}
+	if (!is_root) {
+		cwd.append(" ");
+	}
+}
+
+void replaceHomePath(string & cwd) {
+	string home;
+	size_t index = 0;
+	home = getenv(homeEnvironment);
+	index = cwd.find(home, index);
+	if (index == 0) {
+		cwd.replace(index, home.length(), homeStart);
+	}
+}
+
+void addCurrentWorkingDirectory(string & shellPrompt) {
+	string cwd = getcwd(NULL, 0);
+	replaceHomePath(cwd);
+	replaceSeperators(cwd);
+	shellPrompt += startColorFG(colors::pathFG);
+	shellPrompt += startColorBG(colors::pathBG);
+	shellPrompt += cwd;
+	shellPrompt += resetColors;
+	shellPrompt += startColorFG(colors::pathBG);
+	shellPrompt += seperator;
+	shellPrompt += resetColors;
+}
+
+void addPrompt(string & shellPrompt) {
+	shellPrompt += startColorBG(colors::promptBG);
+	shellPrompt += promptStart;
+	shellPrompt += resetColors;
+	shellPrompt += startColorFG(colors::promptBG);
+	shellPrompt += seperator;
+	shellPrompt += resetColors;
+}
+
+int main()
+{
+	string shellPrompt;
+	shellPrompt.reserve(averagePromptSize);
+	addCurrentWorkingDirectory(shellPrompt);
+	addPrompt(shellPrompt);
+	std::cout << shellPrompt;
+	return 0;
+}
