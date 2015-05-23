3f02212567cbe72685e46dc1e5340f9dceb6af69
ozelentok <oz.elen@gmail.com>
2015-05-23 19:19:40 +0300

    git config file will now be copied instead of linked

--- modules/git/install.sh
+++ modules/git/install.sh
@@ -9,6 +9,6 @@ DIR=$(dirname "${BASH_SOURCE[0]}")
 DIR=$(cd -P $DIR && pwd)
 (
 	cd $DIR
-	ln -s $PWD/gitconfig $HOME/.gitconfig
+	cp $PWD/gitconfig $HOME/.gitconfig
 	ln -s $PWD/gitignore_global $HOME/.gitignore_global
 )
