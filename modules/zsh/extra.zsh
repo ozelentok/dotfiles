268a5dcd6f659ae32b860f10713b5aa5558a22c0
ozelentok <oz.elen@gmail.com>
2015-07-17 18:55:26 +0300

    added functions to extra.zsh

--- modules/zsh/extra.zsh
+++ modules/zsh/extra.zsh
@@ -2,8 +2,19 @@ alias pwf='sudo poweroff'
 alias rbt='sudo reboot'
 alias mtp-mount='simple-mtpfs /mnt/mtp'
 alias mtp-umount='fusermount -u /mnt/mtp'
+alias cpptags='ctags -R --c++-kinds=+p --fields=+iaS --extra=+q --language-force=C++'
+
 #[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx
 
 rdp() {
-	rdesktop $1 -g 1600x900 -u $2 -z -m -x m -0
+	rdesktop $1 -g 1920x1060 -u $2 -z -m -x m -0
+}
+
+startinx() {
+	export DISPLAY=:0
+	$1
+}
+
+sftpmount() {
+	sshfs $1@$2:$3 /mnt/sftp
 }
