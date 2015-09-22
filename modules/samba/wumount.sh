59c28ecba52c8dc77c309f5101bb796c531765ce
Oz Elentok <oz.elen@gmail.com>
2015-09-22 11:59:15 +0300

    Added scripts(wmount/wumount) to control and automate CIFS mount and unmounts

---
+++ modules/samba/wumount.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+if [ "$#" -ne 2 ]; then
+	echo "wumount - Unmount CIFS Shares from /mnt/cifs"
+	echo "Usage: wumount [HOST] [REMOTE DIR]"
+	echo ""
+	exit 1
+fi
+REMOTE_HOST=$1
+REMOTE_DIR=$2
+sudo umount /mnt/cifs/$REMOTE_HOST/$REMOTE_DIR
+sudo rmdir /mnt/cifs/$REMOTE_HOST/$REMOTE_DIR
+sudo rmdir --ignore-fail-on-non-empty /mnt/cifs/$REMOTE_HOST
+echo "Share dismounted from /mnt/cifs/$REMOTE_HOST/$REMOTE_DIR"
