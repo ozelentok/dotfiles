97886b535d0f7f841d17b114a8190f6e50c59135
Oz Elentok <oz.elen@gmail.com>
2015-09-22 11:59:15 +0300

    Added scripts(wmount/wumount) to control and automate CIFS mount and unmounts

--- modules/samba/install.sh
+++ modules/samba/install.sh
@@ -9,8 +9,10 @@ DIR=$(dirname "${BASH_SOURCE[0]}")
 DIR=$(cd -P $DIR && pwd)
 TODIR="/etc/samba"
 (
-	sudo cd $DIR
+	cd $DIR
 	sudo mkdir -p $TODIR
-	sudo sh -c "sed -e \"s/HOST_NAME/$HOST/\" smb.conf > $TODIR"
+	sudo sh -c "sed -e \"s/HOST_NAME/$HOST/\" smb.conf > $TODIR/smb.conf"
+	sudo ln -s $PWD/wmount.sh /usr/local/bin/wmount
+	sudo ln -s $PWD/wumount.sh /usr/local/bin/wumount
 )
 
---
+++ modules/samba/wmount.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+if [ "$#" -ne 3 ]; then
+	echo "wmount - Mount CIFS Shares on /mnt/cifs"
+	echo "Usage: wmount [HOST] [REMOTE DIR] [USER]"
+	echo ""
+	exit 1
+fi
+LOCAL_USER=$USER
+LOCAL_GROUP=$(id -g -n $USER)
+REMOTE_HOST=$1
+REMOTE_DIR=$2
+REMOTE_USER=$3
+sudo mkdir -p /mnt/cifs/$REMOTE_HOST/$REMOTE_DIR
+set +e
+sudo mount -t cifs //$REMOTE_HOST/$REMOTE_DIR \
+/mnt/cifs/$REMOTE_HOST/$REMOTE_DIR -o user=$REMOTE_USER,\
+rw,uid=$LOCAL_USER,gid=$LOCAL_GROUP
+MOUNT_EXIT_CODE=$?
+set -e
+if [ $MOUNT_EXIT_CODE -ne 0 ]; then
+	sudo rmdir /mnt/cifs/$REMOTE_HOST/$REMOTE_DIR
+	sudo rmdir --ignore-fail-on-non-empty /mnt/cifs/$REMOTE_HOST
+else
+	echo "Share mounted on /mnt/cifs/$REMOTE_HOST/$REMOTE_DIR"
+fi
