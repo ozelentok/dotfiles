a53c3dda55bc90149aa5d8b64283a39a7918fcd0
Oz Elentok <oz.elen@gmail.com>
2016-06-03 10:34:32 +0300

    Add 'psu' alias for displaying usermode processes only

--- modules/zsh/extra.zsh
+++ modules/zsh/extra.zsh
@@ -3,6 +3,7 @@ alias rbt='sudo reboot'
 alias mtp-mount='simple-mtpfs /mnt/mtp'
 alias mtp-umount='fusermount -u /mnt/mtp'
 alias cpptags='ctags -R --c++-kinds=+p --fields=+iaS --extra=+q --language-force=C++'
+alias psu='ps -lF --ppid 2 -p 2 --deselect'
 
 rdp() {
 	rdesktop $1 -g 1920x1060 -u $2 -z -m -x m -0
