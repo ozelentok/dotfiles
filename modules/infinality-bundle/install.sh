1e3ae09159c55ddf3d09eb54d97ec192af054512
Oz Elentok <oz.elen@gmail.com>
2016-10-19 18:50:33 +0300

    Add Infinality-Bundle module for imporved font rendering

---
+++ modules/infinality-bundle/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo ""
+echo "=============================="
+echo "Configuring Infinality-Bundle (Better Font Rendering)"
+
+DIR=$(dirname "${BASH_SOURCE[0]}")
+DIR=$(cd -P $DIR && pwd)
+
+(
+	cd $DIR
+	cat $DIR/pacman-infinality.conf >> /etc/pacman.conf
+	sudo pacman-key -r 962DDE58
+	sudo pacman-key --lsign-key 962DDE58
+	sudo pacman -Syu infinality-bundle infinality-bundle-multilib infinality-bundle-fonts
+)
