524eeac58443b12bda8d7168b59edc9925f330e0
Oz Elentok <oz.elen@gmail.com>
2016-11-24 21:51:32 +0200

    Add dconf defualt settings for gnome apps such as gnome-terminal and antialiasing settings

--- modules/gnome/install.sh
+++ modules/gnome/install.sh
@@ -12,6 +12,9 @@ sudo pacman -S gnome gnome-tweak-tool gnome-settings-daemon
 
 (
 	cd $DIR
-	sudo gsettings set org.gnome.settings-daemon.plugins.cursor active false
-	sudo gsettings set org.gnome.settings-daemon.plugins.keyboard active false
+	dconf load / < dconf.ini 
+
+	# no longer needed on new versions
+	#sudo gsettings set org.gnome.settings-daemon.plugins.cursor active false
+	#sudo gsettings set org.gnome.settings-daemon.plugins.keyboard active false
 )
