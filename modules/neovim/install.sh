33bc9df2e2b214244f48c608f2b784f3b4772c56
Oz Elentok <oz.elen@gmail.com>
2016-12-14 00:41:46 +0200

    Fix neovim script typos

--- modules/neovim/install.sh
+++ modules/neovim/install.sh
@@ -19,9 +19,10 @@ sudo pacman -S clang
 (
 	cd $DIR
 	mkdir -p $HOME/.config/nvim/dein/repos/github.com/Shougo/
-	ln -s $DIR/init.vim $HOME/config/nvim/init.vim
+	ln -s $DIR/init.vim $HOME/.config/nvim/init.vim
 	git clone https://github.com/Shougo/dein.vim.git $HOME/.config/nvim/dein/repos/github.com/Shougo/dein.vim
 	mkdir -p $HOME/.config/nvim/colors
 	cp colosus.vim $HOME/.config/nvim/colors/colosus.vim
 	echo "Enter ':call dein#install()'" | nvim -
+	echo ""
 )
