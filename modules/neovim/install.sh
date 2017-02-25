1a537b91d03965eb4845485fb0a8451858fbd087
Oz Elentok <oz.elen@gmail.com>
2017-02-25 11:31:50 +0200

    Add GNU Global for tags and cscope functionality in neovim

--- modules/neovim/install.sh
+++ modules/neovim/install.sh
@@ -8,23 +8,26 @@ echo "========================================"
 DIR=$(dirname "${BASH_SOURCE[0]}")
 DIR=$(cd -P $DIR && pwd)
 
-sudo pacman -S neovim
+#clang for Deoplete C/C++ Support
+#xsel and xclip for Clipboard support 
+sudo pacman -S neovim clang xsel xclip
 sudo pip install neovim 
 sudo pip2 install neovim
 
-#For Deoplete C/C++ Support
-sudo pacman -S clang
-#For X Clipboard support 
-sudo pacman -S xsel xclip
-
-
 (
 	cd $DIR
+	./gnu-global/install.sh
+
 	mkdir -p $HOME/.config/nvim/dein/repos/github.com/Shougo/
 	ln -s $DIR/init.vim $HOME/.config/nvim/init.vim
 	git clone https://github.com/Shougo/dein.vim.git $HOME/.config/nvim/dein/repos/github.com/Shougo/dein.vim
+
 	mkdir -p $HOME/.config/nvim/colors
 	cp colosus.vim $HOME/.config/nvim/colors/colosus.vim
+
+	mkdir -p $HOME/.config/nvim/plugin
+	cp /usr/share/vim/vimfiles/plugin/gtags* $HOME/.config/nvim/plugin
+
 	echo "Enter ':call dein#install()'" | nvim -
 	echo ""
 )
