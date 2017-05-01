2ea8ef429b2d533e53f06c9f4ab64a20b042a0a9
Oz Elentok <oz.elen@gmail.com>
2017-05-01 17:05:30 +0300

    Add missing gtags environment variable for pygments plugin support

--- modules/neovim/gnu-global/install.sh
+++ modules/neovim/gnu-global/install.sh
@@ -17,4 +17,7 @@ DIR=$(cd -P $DIR && pwd)
 
 	ln -s $DIR/globalrc ~/.globalrc
 	sudo sed -i '1s/python/python2/' /usr/share/gtags/script/pygments_parser.py
+	echo ""
+	echo "Remember to set GTAGSLABEL=pygments in your shell startup file"
+	echo " (The Zsh module already includes this configuration)"
 )
