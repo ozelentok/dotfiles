4db52bb92790a2a56ca536da8fbb70fbc9160a50
Oz Elentok <oz.elen@gmail.com>
2018-11-15 23:35:06 +0200

    Sync clipboards using autocutsel

--- modules/i3/install.sh
+++ modules/i3/install.sh
@@ -8,13 +8,13 @@ echo "========================================"
 DIR=$(dirname "${BASH_SOURCE[0]}")
 DIR=$(cd -P $DIR && pwd)
 
-sudo pacman -S i3-wm i3lock i3status dmenu feh
+sudo pacman -S i3-wm i3lock i3status dmenu feh autocutsel
 
 (
 	cd $DIR
 	TODIR="$HOME/.config/i3/"
 	mkdir -p $TODIR
 	ln -s $DIR/config $TODIR
-	ln -s $DIR/i3status.conf $TODIR 
-	ln -s $DIR/i3-session $TODIR 
+	ln -s $DIR/i3status.conf $TODIR
+	ln -s $DIR/i3-session $TODIR
 )
