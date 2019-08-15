709757010eb1e0bb0c712817da1385e1f9e5bd00
Oz Elentok <oz.elen@gmail.com>
2019-08-15 20:27:40 +0300

    Add GTK3 and GTK2 dark theme configurations

--- modules/gnome/install.sh
+++ modules/gnome/install.sh
@@ -8,9 +8,13 @@ echo "========================================"
 DIR=$(dirname "${BASH_SOURCE[0]}")
 DIR=$(cd -P $DIR && pwd)
 
-sudo pacman -Syu --needed gnome-terminal gnome-themes-extra eog evince
+sudo pacman -Syu --needed gtk2 gtk3 gnome-terminal gnome-themes-extra eog evince
 
 (
 	cd $DIR
 	dconf load / < dconf.ini
+
+	mkdir -p $HOME/.config/gtk-3.0
+	ln -s -f -r $DIR/gtk-3.0-settings.ini $HOME/.config/gtk-3.0/settings.ini
+	ln -s -f -r $DIR/gtkrc-2.0 $HOME/.gtkrc-2.0
 )
