da90537b2ade389e39b9ce89d278b2019ff5f795
Oz Elentok <oz.elen@gmail.com>
2019-10-11 19:54:33 +0300

    Install QT5 Style Plugins to enable emulation of GTK Theme

--- modules/qtconfig/install.sh
+++ modules/qtconfig/install.sh
@@ -8,6 +8,8 @@ echo "========================================"
 DIR=$(dirname "${BASH_SOURCE[0]}")
 DIR=$(cd -P $DIR && pwd)
 
+sudo pacman -Syu --needed qt5-styleplugins
+
 (
 	mkdir -p "$HOME/.config"
 	cp $DIR/Trolltech.conf $HOME/.config/Trolltech.conf
