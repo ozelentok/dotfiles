98cd7251d33c6da27a900004617a4b3ac233802d
Oz Elentok <oz.elen@gmail.com>
2020-03-10 10:08:34 +0200

    Refactor mount scripts

--- modules/mount-utils/sftpumount.sh
+++ modules/mount-utils/sftpumount.sh
@@ -2,15 +2,13 @@
 
 set -e
 
-if [ "$#" -lt 2 ] || [ "$#" -gt 3 ]; then
+if [ "$#" -lt 1 ]; then
 	echo "sftpumount - Unmount SFTP Share from /mnt/sftp"
-	echo "Usage: sftpumount host local_dir_name"
+	echo "Usage: sftpumount [HOST]"
 	echo ""
 	exit 1
 fi
 REMOTE_HOST=$1
-DIR=$2
-fusermount -u /mnt/sftp/$REMOTE_HOST/$DIR
-rmdir /mnt/sftp/$REMOTE_HOST/$DIR
-rmdir --ignore-fail-on-non-empty /mnt/sftp/$REMOTE_HOST
-echo "Share dismounted from /mnt/sftp/$REMOTE_HOST/$DIR"
+fusermount -u /mnt/sftp/$REMOTE_HOST
+rmdir /mnt/sftp/$REMOTE_HOST
+echo "Share dismounted from /mnt/sftp/$REMOTE_HOST"
