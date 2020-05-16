a701dd6232beed4ed3e4d66c71bd2a5de30d05b6
Oz Elentok <oz.elen@gmail.com>
2020-05-16 19:56:40 +0300

    Use Arc Black Classic Theme / Arc Icons / Oxy Neon Cursor

--- modules/gnome/install.sh
+++ modules/gnome/install.sh
@@ -8,13 +8,21 @@ echo "========================================"
 DIR=$(dirname "${BASH_SOURCE[0]}")
 DIR=$(cd -P $DIR && pwd)
 
-sudo pacman -Syu --needed gtk2 gtk3 gnome-terminal gnome-themes-extra eog evince
+sudo pacman -Syu --needed gtk2 gtk3 gnome-terminal eog evince
 
 (
 	cd $DIR
-	dconf load / < dconf.ini
+	mkdir -p $HOME/.themes
+	mkdir -p $HOME/.local/share/icons
+	ln -s -f -r $HOME/.local/share/icons $HOME/.icons
+
+	tar -C $HOME/.themes -xf ./Arc-Black-Classic-3.36.tar.xz 
+	tar -C $HOME/.local/share/icons -xf ./Arc-ICONS.tar.xz
+	tar -C $HOME/.local/share/icons -xf ./oxy-neon.tar.gz
 
 	mkdir -p $HOME/.config/gtk-3.0
+	dconf load / < dconf.ini
+
 	ln -s -f -r $DIR/gtk-3.0-settings.ini $HOME/.config/gtk-3.0/settings.ini
 	ln -s -f -r $DIR/gtkrc-2.0 $HOME/.gtkrc-2.0
 )
