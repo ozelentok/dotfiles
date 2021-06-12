44c9e0e3ed9e7b47ae29df771d32c0cc469df7c0
Oz Elentok <oz.elen@gmail.com>
2021-06-12 23:00:36 +0300

    Use blue-cyan color for i3 status
    Recolorize neovim theme
    Use nvim-colorizer instead of vim-css-color
    Use new visual block copy-paste mapping
    Replace 'ls' with 'lsd'

--- modules/zsh/install.sh
+++ modules/zsh/install.sh
@@ -7,7 +7,7 @@ echo "Installing ZSH"
 DIR=$(dirname "${BASH_SOURCE[0]}")
 DIR=$(cd -P $DIR && pwd)
 
-sudo pacman -Syu --needed zsh zsh-completions gcc make
+sudo pacman -Syu --needed zsh zsh-completions lsd gcc make
 
 (
 	cd $DIR
