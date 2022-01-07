ea71582cfd52e298d008beaf3d17d7bde6f476a0
Oz Elentok <oz.elen@gmail.com>
2022-01-07 19:09:49 +0200

    chore(nvim): update material.nvim

--- dotfiles/neovim/lua/init/colors.lua
+++ dotfiles/neovim/lua/init/colors.lua
@@ -1,12 +1,15 @@
 vim.g.material_style = 'darker'
 require('material').setup({
-	contrast = true,
-	borders = true,
-	popup_menu = 'dark',
-	italics = {comments = true},
-	text_contrast = {darker = true},
-	disable = {background = true},
-	custom_colors = {fg = '#FFFFFF', line_numbers = '#c4a000'}
+	custom_colors = {
+		fg = '#FFFFFF',
+		line_numbers = '#c4a000'
+	},
+	high_visibility = {
+		darker = true
+	},
+	disable = {
+		background = true
+	},
 })
 vim.cmd[[colorscheme material]]
 require('colorizer').setup()
