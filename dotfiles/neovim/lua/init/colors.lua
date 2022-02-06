d8473a502879f902ef204ab7a00742417deec110
Oz Elentok <oz.elen@gmail.com>
2022-02-06 22:36:40 +0200

    fix(neovim): set darker backgrounds for material theme

--- dotfiles/neovim/lua/init/colors.lua
+++ dotfiles/neovim/lua/init/colors.lua
@@ -1,7 +1,20 @@
 vim.g.material_style = 'darker'
 require('material').setup({
+	contrast = {
+		sidebars = true,
+		floating_windows = true,
+		line_numbers = true,
+		sign_column = true,
+		cursor_line = true,
+		non_current_windows = false,
+		popup_menu = true,
+	},
 	custom_colors = {
 		fg = '#FFFFFF',
+		bg = '#000000',
+		bg_alt = '#303030',
+		bg_num = '#161616',
+		bg_sign = '#161616',
 		line_numbers = '#c4a000'
 	},
 	high_visibility = {
