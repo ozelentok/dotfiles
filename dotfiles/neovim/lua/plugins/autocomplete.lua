5ca3e50bd7f5d95c3d5bdfb4abb5cfddc9250478
Oz Elentok <oz.elen@gmail.com>
2023-11-30 01:56:54 +0200

    feat(nvim): Remap nvim-cmp keybindings
    Map Up and Down arrows for navigation
    Ctrl-N and Ctrl-P navigate and invoke completion if not already open
    Enter will expand or complete only when completion is open but will always advance to next snippet tabstop
    Tab and Shift-Tab will jump between snippets tabstops
    ESC will only close the completion in command-line mode

--- dotfiles/neovim/lua/plugins/autocomplete.lua
+++ dotfiles/neovim/lua/plugins/autocomplete.lua
@@ -22,25 +22,51 @@ return {
           end,
         },
         mapping = {
-          ['<C-p>'] = cmp.mapping(function()
-            cmp.select_prev_item()
+          ['<Down>'] = cmp.mapping(function()
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
           end, { 'i', 's', 'c' }),
+          ['<Up>'] = cmp.mapping(function()
+            cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+          end, { 'i', 's', 'c' }),
+
           ['<C-n>'] = cmp.mapping(function()
-            cmp.select_next_item()
-          end, { 'i', 's', 'c' }),
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping({
-            i = cmp.mapping.abort(),
-            c = cmp.mapping.close(),
-          }),
-          ['<CR>'] = cmp.mapping.confirm({ select = false }),
-          ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
-              cmp.select_next_item()
-            elseif snippy.can_expand_or_advance() then
-              snippy.expand_or_advance()
+              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+            else
+              cmp.complete()
+            end
+          end, { 'i', 's', 'c' }),
+          ['<C-p>'] = cmp.mapping(function()
+            if cmp.visible() then
+              cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+            else
+              cmp.complete()
+            end
+          end, { 'i', 's', 'c' }),
+
+          ['<CR>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              if cmp.get_selected_entry() then
+                cmp.confirm({ select = false })
+              elseif snippy.can_expand_or_advance() then
+                snippy.expand_or_advance()
+              else
+                fallback()
+              end
+            elseif snippy.can_jump(1) then
+              snippy.next()
+            else
+              fallback()
+            end
+          end, { 'i', 's', 'c' }),
+
+          ['<ESC>'] = {
+            c = cmp.mapping.close(),
+          },
+
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if snippy.can_jump(1) then
+              snippy.next()
             else
               fallback()
             end
@@ -48,12 +74,13 @@ return {
           ['<S-Tab>'] = cmp.mapping(function(fallback)
             if snippy.can_jump(-1) then
               snippy.previous()
-            elseif cmp.visible() then
-              cmp.select_prev_item()
             else
               fallback()
             end
           end, { 'i', 's' }),
+
+          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
         },
         sources = cmp.config.sources({
           { name = 'snippy' },
@@ -71,14 +98,12 @@ return {
           },
         })
       })
-      cmp.setup.cmdline('/', {
-        mapping = cmp.mapping.preset.cmdline(),
+      cmp.setup.cmdline('/', { ---@diagnostic disable-line: undefined-field
         sources = cmp.config.sources({
           { name = 'buffer' }
         })
       })
-      cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
+      cmp.setup.cmdline(':', { ---@diagnostic disable-line: undefined-field
         sources = cmp.config.sources({
           { name = 'path' }
         }, {
