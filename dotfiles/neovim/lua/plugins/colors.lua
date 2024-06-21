18f7d15d2512a4367171a5ff949390eaea4941a5
Oz Elentok <oz.elen@gmail.com>
2024-06-21 19:18:00 +0300

    chore(nvim): Lighter shade of green for strings and cyan for folding headers

--- dotfiles/neovim/lua/plugins/colors.lua
+++ dotfiles/neovim/lua/plugins/colors.lua
@@ -2,7 +2,7 @@ local colors = {
   black       = '#000000',
   fg          = '#F0F0F0',
   purple      = '#D070F0',
-  green       = "#20E040",
+  green       = "#30F040",
   yellow      = '#F0D020',
   cyan        = '#50E0F9',
   grey        = '#708090',
@@ -31,6 +31,7 @@ return {
         Search = { fg = colors.black, bg = colors.yellow, bold = false },
         LineNr = { fg = colors.dark_cyan },
         MatchParen = { fg = colors.black, bg = colors.purple },
+        Folded = { fg = colors.cyan },
       },
       diagnostics = {
         darker = false,
