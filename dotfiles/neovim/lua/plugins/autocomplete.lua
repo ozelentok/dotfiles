212c039eba189d0df4fef0347532b558e46e699e
Oz Elentok <oz.elen@gmail.com>
2025-04-29 23:48:36 +0300

    feat(nvim): Add `blink.cmp`
    Remove `nvim-cmp`, `nvim-snippy` and `nvim-autopairs`
    Set `blink.cmp` custom keymap
    Use `vim.lsp.enable` instead of nvim-lspconfig `setup`
    Use 'friendly-snippets' as snippets source
    Convert custom snippets to vscode-style snippets as required by `vim.snippet`

--- dotfiles/neovim/lua/plugins/autocomplete.lua
+++ dotfiles/neovim/lua/plugins/autocomplete.lua
@@ -1,192 +1,39 @@
 return {
   {
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-nvim-lua',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-cmdline',
-      'dcampos/cmp-snippy',
-      'windwp/nvim-autopairs',
+    'saghen/blink.cmp',
+    dependencies = { 'rafamadriz/friendly-snippets' },
+    version = '1.*',
+    opts = {
+      keymap = {
+        preset = 'none',
+        ['<Up>'] = { 'select_prev', 'fallback' },
+        ['<Down>'] = { 'select_next', 'fallback' },
+        ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+        ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+        ['<C-Space>'] = { 'select_next', 'fallback' },
+        ['ESC'] = { 'cancel', 'fallback' },
+        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+        ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
+        ['<C-m>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        ['<CR>'] = { 'accept', 'snippet_forward', 'fallback' },
+        ['<Tab>'] = { 'snippet_forward', 'fallback' },
+        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+      },
+      completion = {
+        documentation = { auto_show = true },
+        list = {
+          selection = {
+            preselect = false,
+            auto_insert = false,
+          }
+        }
+      },
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+      fuzzy = { implementation = 'prefer_rust_with_warning' },
+      signature = { enabled = true }
     },
-    config = function()
-      local cmp = require('cmp')
-      local snippy = require('snippy')
-      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-
-      cmp.setup({ ---@diagnostic disable-line: redundant-parameter
-        snippet = {
-          expand = function(args)
-            snippy.expand_snippet(args.body)
-          end,
-        },
-        mapping = {
-          ['<Down>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<Up>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-
-          ['<C-Space>'] = cmp.mapping(function()
-            if cmp.visible() then
-              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-            else
-              cmp.complete()
-            end
-          end, { 'i', 's', 'c' }),
-          ['<C-n>'] = cmp.mapping(function()
-            if cmp.visible() then
-              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-            else
-              cmp.complete()
-            end
-          end, { 'i', 's', 'c' }),
-          ['<C-p>'] = cmp.mapping(function()
-            if cmp.visible() then
-              cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-            else
-              cmp.complete()
-            end
-          end, { 'i', 's', 'c' }),
-
-          ['<CR>'] = cmp.mapping({
-            i = function(fallback)
-              if cmp.visible() then
-                if cmp.get_selected_entry() then
-                  cmp.confirm({ select = false })
-                elseif snippy.can_expand_or_advance() then
-                  snippy.expand_or_advance()
-                else
-                  fallback()
-                end
-              elseif snippy.can_jump(1) then
-                snippy.next()
-              else
-                fallback()
-              end
-            end,
-            s = function(fallback)
-              if cmp.visible() and cmp.get_selected_entry() then
-                cmp.confirm({ select = false })
-              else
-                fallback()
-              end
-            end,
-            c = function(fallback)
-              if cmp.visible() and cmp.get_selected_entry() then
-                cmp.confirm({ select = false })
-              else
-                fallback()
-              end
-            end,
-          }),
-
-          ['<ESC>'] = {
-            c = cmp.mapping.close(),
-          },
-
-          ['<Tab>'] = cmp.mapping({
-            i = function(fallback)
-              if snippy.can_jump(1) then
-                snippy.next()
-              else
-                fallback()
-              end
-            end,
-            s = function(fallback)
-              if snippy.can_jump(1) then
-                snippy.next()
-              else
-                fallback()
-              end
-            end,
-            c = function(fallback)
-              if cmp.visible() then
-                cmp.select_next_item({ behavior = cmp.SelectBehavior.Replace })
-              else
-                fallback()
-              end
-            end,
-          }),
-
-          ['<S-Tab>'] = cmp.mapping({
-            i = function(fallback)
-              if snippy.can_jump(-1) then
-                snippy.previous()
-              else
-                fallback()
-              end
-            end,
-            s = function(fallback)
-              if snippy.can_jump(-1) then
-                snippy.previous()
-              else
-                fallback()
-              end
-            end,
-            c = function(fallback)
-              if cmp.visible() then
-                cmp.select_prev_item({ behavior = cmp.SelectBehavior.Replace })
-              else
-                fallback()
-              end
-            end,
-          }),
-
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-m>'] = function()
-            if cmp.visible_docs() then
-              cmp.close_docs()
-            else
-              cmp.open_docs()
-            end
-          end,
-        },
-        sources = cmp.config.sources({
-          { name = 'snippy' },
-          { name = 'nvim_lsp' },
-          { name = 'nvim_lua' },
-          { name = 'buffer' },
-          {
-            name = 'path',
-            option = {
-              label_trailing_slash = false,
-              get_cwd = function()
-                return vim.fn.getcwd(vim.fn.winnr())
-              end,
-            }
-          },
-        })
-      })
-      cmp.setup.cmdline('/', { ---@diagnostic disable-line: undefined-field
-        sources = cmp.config.sources({
-          { name = 'buffer' }
-        })
-      })
-      cmp.setup.cmdline(':', { ---@diagnostic disable-line: undefined-field
-        sources = cmp.config.sources({
-          { name = 'path' }
-        }, {
-          { name = 'cmdline', option = { ignore_cmds = { 'Man', '!' } } }
-        })
-      })
-      cmp.event:on(
-        'confirm_done',
-        cmp_autopairs.on_confirm_done()
-      )
-    end,
   },
-  'dcampos/nvim-snippy',
-  'honza/vim-snippets',
 }
--- dotfiles/neovim/lua/plugins/lsp.lua
+++ dotfiles/neovim/lua/plugins/lsp.lua
@@ -3,27 +3,14 @@ vim.lsp.set_log_level('OFF')
 return {
   {
     'neovim/nvim-lspconfig',
-    dependencies = {
-      'ray-x/lsp_signature.nvim',
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/nvim-cmp',
-    },
     event = { 'BufReadPost', 'BufNewFile' },
     keys = {
       { '<leader>v', vim.lsp.buf.format,      noremap = true, desc = 'LSP Format' },
       { '<leader>c', vim.lsp.buf.code_action, noremap = true, desc = 'LSP Code Action' },
     },
     config = function()
-      local lspconfig = require('lspconfig')
-      local on_lsp_attach = function(_, bufnr)
-        vim.api.nvim_set_option_value('omnifunc', 'v:lua.vim.lsp.omnifunc', { buf = bufnr })
-        require('lsp_signature').on_attach({ hint_enable = false, hi_parameter = 'MoreMsg' }, bufnr)
-      end
-
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      lspconfig.lua_ls.setup {
-        on_attach = on_lsp_attach,
-        capabilities = capabilities,
+      vim.lsp.enable('lua_ls')
+      vim.lsp.config('lua_ls', {
         settings = {
           Lua = {
             runtime = { version = 'LuaJIT', },
@@ -41,34 +28,22 @@ return {
             }
           }
         }
-      }
+      })
 
-      lspconfig.html.setup({ on_attach = on_lsp_attach, capabilities = capabilities })
-      lspconfig.jsonls.setup({ on_attach = on_lsp_attach, capabilities = capabilities })
-      lspconfig.cssls.setup({ on_attach = on_lsp_attach, capabilities = capabilities })
-      lspconfig.clangd.setup({ on_attach = on_lsp_attach, capabilities = capabilities })
-      lspconfig.pyright.setup({ on_attach = on_lsp_attach, capabilities = capabilities })
-      lspconfig.ruff.setup({ on_attach = on_lsp_attach, capabilities = capabilities })
-      lspconfig.eslint.setup({ on_attach = on_lsp_attach, capabilities = capabilities })
-      lspconfig.cmake.setup({ on_attach = on_lsp_attach, capabilities = capabilities })
-      lspconfig.bashls.setup({
-        filetypes = { 'sh', 'zsh' },
-        on_attach = on_lsp_attach,
-        capabilities = capabilities
-      })
-      lspconfig.arduino_language_server.setup({ on_attach = on_lsp_attach, capabilities = capabilities })
-      lspconfig.openscad_lsp.setup({
-        cmd = { 'openscad-lsp', '--stdio', '--fmt-style', 'LLVM' },
-        on_attach = on_lsp_attach,
-        capabilities = capabilities,
-      })
+      vim.lsp.enable('clangd')
+      vim.lsp.enable('pyright')
+      vim.lsp.enable('ruff')
+      vim.lsp.enable('eslint')
+      vim.lsp.enable('cmake')
+      vim.lsp.enable('html')
+      vim.lsp.enable('jsonls')
+      vim.lsp.enable('cssls')
+      vim.lsp.enable('arduino_language_server')
+      vim.lsp.enable('bashls')
+      vim.lsp.enable('openscad_lsp')
+      vim.lsp.config('bashls', { filetypes = { 'bash', 'sh', 'zsh' } })
+      vim.lsp.config('openscad_lsp', { cmd = { 'openscad-lsp', '--stdio', '--fmt-style', 'LLVM' } })
     end
-  },
-  {
-    'ray-x/lsp_signature.nvim',
-    keys = {
-      { '<C-,>', function() require('lsp_signature').toggle_float_win() end, mode = 'i', noremap = true },
-    }
   },
   {
     'nvimtools/none-ls.nvim',
