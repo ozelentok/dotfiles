da3d3c14c35bfbc628e3bded62ec08750ca1df3e
Oz Elentok <oz.elen@gmail.com>
2025-05-24 14:25:07 +0300

    chore(fontconfig): Use `Nerd Font Symbols` for icons instead of the patched `Hack` font

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require('wezterm')
 
 return {
-  font = wezterm.font_with_fallback({ 'Hack Nerd Font', 'Liberation Sans', 'Noto Color Emoji' }),
+  font = wezterm.font_with_fallback({ 'Hack', 'Liberation Mono', 'Symbols Nerd Font', 'Noto Color Emoji' }),
   font_size = 10.5,
   front_end = 'WebGpu',
   max_fps = 120,
